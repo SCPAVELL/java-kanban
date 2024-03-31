bd1fe64ccc9afcb44a0c6c7833c06eb423f17b52
Pavel <pavel2001stepanov@yandex.ru>
2024-03-31 15:01:37 +0300

    fixed Local varible

--- src/task/Task.java
+++ src/task/Task.java
@@ -69,8 +69,8 @@ public class Task {
 	}
 
 	public Instant getEndTime() {
-		long SECONDS_IN_MINUTE = 60L;
-		return startTime.plusSeconds(duration * SECONDS_IN_MINUTE);
+		long SecondsInMinune = 60L;
+		return startTime.plusSeconds(duration * SecondsInMinune);
 	}
 
 	@Override
