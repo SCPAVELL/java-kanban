e8dfd4665f70795306db03c421698d844791f16b
Pavel <pavel2001stepanov@yandex.ru>
2024-03-31 15:20:58 +0300

    remove import

--- src/test/TaskManagerTest.java
+++ src/test/TaskManagerTest.java
@@ -1,9 +1,9 @@
 package test;
 
 import static org.junit.Assert.assertTrue;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
 
 import java.time.Instant;
 import java.util.Collections;
