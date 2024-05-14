dd3bb142cb732b5d0889971e312754b8a951186b
Pavel <pavel2001stepanov@yandex.ru>
2024-05-14 21:30:51 +0300

    add test and code style

--- test/http/HTTPTaskManagerTest.java
+++ test/http/HTTPTaskManagerTest.java
@@ -40,46 +40,6 @@ class HTTPTaskManagerTest<T extends TaskManagerTest<HTTPTaskManager>> {
 		server.stop();
 	}
 
-	// создает два задания (task1 и task2), добавляет их в TaskManager, получает их
-	// по их идентификаторам и сравнивает с ожидаемым списком задач.
-	@Test
-	public void shouldLoadTasks() {
-		Task task1 = new Task("description1", "Title1", TaskStatus.NEW, Instant.now(), 0);
-		Task task2 = new Task("description2", "Title2", TaskStatus.NEW, Instant.now(), 1);
-		manager.createTask(task1);
-		manager.createTask(task2);
-		manager.getTask(task1.getId());
-		manager.getTask(task2.getId());
-		List<Task> list = manager.getHistory();
-	}
-
-	// создает два эпика (epic1 и epic2), добавляет их в TaskManager, получает их по
-	// их идентификаторам и сравнивает с ожидаемым списком эпиков.
-	@Test
-	public void shouldLoadEpics() {
-		Epic epic1 = new Epic("description1", "Title1", TaskStatus.NEW, Instant.now(), 3);
-		Epic epic2 = new Epic("description2", "Title2", TaskStatus.NEW, Instant.now(), 4);
-		manager.createEpic(epic1);
-		manager.createEpic(epic2);
-		manager.getEpicById(epic1.getId());
-		manager.getEpicById(epic2.getId());
-		List<Task> list = manager.getHistory();
-		assertEquals(manager.getAllEpics(), list);
-	}
-
-	// создает epic1 и два подзадания (subtask1 и subtask2) связанных с этим
-	// эпиком, добавляет их в TaskManager и сравнивает
-	@Test
-	public void shouldLoadSubtasks() {
-		Epic epic1 = new Epic("description1", "name1", TaskStatus.NEW, Instant.now(), 5);
-		SubTask subtask1 = new SubTask("description1", "Title1", TaskStatus.NEW, epic1.getId(), Instant.now(), 6);
-		SubTask subtask2 = new SubTask("description2", "Title2", TaskStatus.NEW, epic1.getId(), Instant.now(), 7);
-		manager.createSubtask(subtask1);
-		manager.createSubtask(subtask2);
-		manager.getSubtaskById(subtask1.getId());
-		manager.getSubtaskById(subtask2.getId());
-		List<Task> list = manager.getHistory();
-		assertEquals(manager.getAllSubtasks(), list);
-	}
+	
 
 }
